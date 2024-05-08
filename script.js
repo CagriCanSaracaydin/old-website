584192c49690336abebdb81abc2d9574e48265f4
Çağrı Can Saraçaydın <136911022+CagriCanSaracaydin@users.noreply.github.com>
2024-05-08 16:23:01 +0300

    improvements

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ new Vue({
         projects: [
             { title: 'Project 1', description: 'Description of Project 1', link: 'project1.html' },
             { title: 'Project 2', description: 'Description of Project 2', link: 'project2.html' }
+            // Add more projects as needed
         ]
     }
 });
 
 document.addEventListener("DOMContentLoaded", function() {
+    AOS.init();
     const navLinks = document.querySelectorAll('.sidebar-nav a');
 
     navLinks.forEach(link => {
