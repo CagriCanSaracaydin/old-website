fde2d7f97ff35a93852c36e74a815410cfc08e4a
Çağrı Can Saraçaydın <136911022+CagriCanSaracaydin@users.noreply.github.com>
2024-05-08 17:56:57 +0300

    Update

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-import { createApp } from 'https://cdn.jsdelivr.net/npm/vue@next/dist/vue.esm-browser.prod.js';
-
-createApp({
-    data() {
-        return {
-            projects: [
-                { title: 'Project 1', description: 'Description of Project 1', link: 'project1.html' },
-                { title: 'Project 2', description: 'Description of Project 2', link: 'project2.html' }
-            ]
-        };
-    }
-}).mount('.project-gallery');
-
 document.addEventListener("DOMContentLoaded", function() {
+    const app = Vue.createApp({
+        data() {
+            return {
+                projects: [
+                    { title: 'Project 1', description: 'Description of Project 1', link: 'project1.html' },
+                    { title: 'Project 2', description: 'Description of Project 2', link: 'project2.html' }
+                ]
+            };
+        }
+    }).mount('.project-gallery');
+
     AOS.init();
     const navLinks = document.querySelectorAll('.sidebar-nav a');
 
