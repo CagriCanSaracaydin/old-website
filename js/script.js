0f72260643b4bea0f165a24911733e73d499d5dd
Çağrı Can Saraçaydın <136911022+CagriCanSaracaydin@users.noreply.github.com>
2024-05-14 22:08:38 +0300

    CSS variables and better directory management

--- js/script.js
+++ js/script.js
@@ -8,22 +8,25 @@ document.addEventListener("DOMContentLoaded", () => {
         mirror: true,         
     });
 
-    document.querySelectorAll('a.nav-link').forEach(link => {
+    const navLinks = document.querySelectorAll('a.nav-link');
+    navLinks.forEach(link => {
         link.addEventListener('click', (e) => {
             e.preventDefault();
-            let target = document.getElementById(this.getAttribute('href').substring(1));
-            target.scrollIntoView({
+            const targetId = e.currentTarget.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            targetElement.scrollIntoView({
                 behavior: 'smooth',
                 block: 'start'
             });
         });
     });
 
+    const navbar = document.getElementById('navbar');
     window.addEventListener('scroll', () => {
         if (window.scrollY > 50) {
-            document.getElementById('navbar').classList.add('active-nav');
+            navbar.classList.add('active-nav');
         } else {
-            document.getElementById('navbar').classList.remove('active-nav');
+            navbar.classList.remove('active-nav');
         }
     });
 });
